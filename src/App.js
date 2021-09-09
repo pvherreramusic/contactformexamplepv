20131cfdc6868c84dde58721687957622faa148c
pvherreramusic <Trachkid@gmail.com>
2021-09-08 20:51:04 -0700

    Added React Native

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import "./App.css";
-
+import { Text, View } from 'react-native';
 import Album from "./Album";
 
   
 function App() {
   return (
+    <View style={{
+      flex: 1,
+      justifyContent: 'flex-start',
+      alignItems: 'center'
+    }}>
   <Album></Album>
+  </View>
   );
 }
   export default App;
